784e68a6c7b3d4e6a59ee033c77edc2e9142146d
Milam <charles.milam@gmail.com>
2015-07-08 22:34:21 -0500

    Clean up formatting of code, and comments. Working on FFC.

--- bonfire-longest-word.js
+++ bonfire-longest-word.js
@@ -1,11 +1,9 @@
 function findLongestWord(str) {;
     var ary = str.split(" ");
-    console.log("ary: " + ary);
     var longest = ary.reduce(function(a, b){
-      console.log("a: " + a);
-      console.log("b: " + b);
       return a.length > b.length ? a  : b;
     });
+    
     return longest.length;
 }
 
