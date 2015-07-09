8291c556ce6abeacd2bf85f4359c2f8b52f4a93e
Milam <charles.milam@gmail.com>
2015-07-08 22:26:20 -0500

    Reduce statement functions.

--- bonfire-longest-word.js
+++ bonfire-longest-word.js
@@ -1,5 +1,12 @@
-function findLongestWord(str) {
-  return str.length;
+function findLongestWord(str) {;
+    var ary = str.split(" ");
+    console.log("ary: " + ary);
+    var longest = ary.reduce(function(a, b){
+      console.log("a: " + a.length);
+      console.log("b: " + b.length);
+      return a.length > b.length ? a.length  : b.length;
+    });
+    return longest;
 }
 
-findLongestWord('The quick brown fox jumped over the lazy dog');
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
