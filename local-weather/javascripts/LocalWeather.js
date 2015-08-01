b4c655a04a67eccc86785bf5ed7496af4edacf8d
Charles Milam <charles.milam@gmail.com>
2015-08-01 03:33:43 -0500

    Add class for LocalWeather. Add object localWeather to hold weather data.

--- local-weather/javascripts/LocalWeather.js
+++ local-weather/javascripts/LocalWeather.js
@@ -1,3 +1,23 @@
+var LocalWeather = function(unitType) {
+  var localWeather = {
+    "city": "",
+    "temp": 0,
+    "humidity": 0,
+    "sky": "";
+    "wind": {
+      "dir": 0;
+      "speed": 0;
+    }
+  }
+
+  
+} // end LocalWeather
+
+
+
+
+
+
 function getWeather(apiUrl) {
   $.getJSON(apiUrl, function(data){
     console.log(data);
