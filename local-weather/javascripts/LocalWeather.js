7cc24db4133951654951746e689300e4dbd05c5b
Charles Milam <charles.milam@gmail.com>
2015-08-01 16:45:53 -0500

    Continue to attempt to get object working properly.

--- local-weather/javascripts/LocalWeather.js
+++ local-weather/javascripts/LocalWeather.js
@@ -1,6 +1,7 @@
 function LocalWeather(unitType) {
   var WEATHER_API_KEY = "87a3ac98e2e48918db144e9f69eeb057";
-  this.localWeather = {
+  var urlApi = "";
+  var localWeather = {
         "city": "",
         "temp": 0,
         "humidity": 0,
@@ -12,13 +13,14 @@ function LocalWeather(unitType) {
       };
 
   this.getLocalWeather = function() {
-    return this.localWeather;
+    return localWeather;
   }
   this.getCity = function() {
-    console.log("get city: " + this.localWeather.city);
-    return this.localWeather.city;
+    console.log("get city: " + localWeather.city);
+    return localWeather.city;
   }
   this.setLocation = function() {
+    var apiUrl = ""
     navigator.geolocation.getCurrentPosition(success, error);
 
     function success(position) {
@@ -36,7 +38,8 @@ function LocalWeather(unitType) {
                 WEATHER_API_KEY;
 
       console.log("after: " + apiUrl);
-      getWeatherFromApi(apiUrl);
+      urlApi = apiUrl;
+      // getWeatherFromApi(apiUrl);
     };
 
     function error(error) {
@@ -44,30 +47,15 @@ function LocalWeather(unitType) {
     };
   }
 
-  function getWeatherFromApi(apiUrl) {
+  function getWeatherFromApi() {
     //console.log(apiUrl);
-    var result = $.getJSON(apiUrl, function(data){
-      // console.log(this);
-      // console.log("in get weather");
-      // // console.log(data.name);
-      // //this.localWeather = data.name;
-      // console.log(this.localWeather);
-      //console.log(data.responseText);
-      // console.log(data.responseText.coord);
-
-      // $.each(data, function(val){
-      //   console.log(val);
-      // });
-      return data;
+    $.getJSON(urlApi, function(data){
+      console.log(data.name);
+      localWeather.city = data.name;
     })
     .fail(function(jqxhr, status, error) {
       var err = status + ", " + error;
       alert("Sorry, the request failed: " + err);
     });
-    $.each(result, function(val){
-      console.log(val);
-    });
-    console.log(result);
-    // console.log(result.responseText);
   }
 } // end LocalWeather
