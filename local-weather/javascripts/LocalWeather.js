6dad5df0f3e46156f0b0a3f81d5ad98a9d5385b8
Charles Milam <charles.milam@gmail.com>
2015-08-01 16:04:44 -0500

    WIP - can't get object setup properly.

--- local-weather/javascripts/LocalWeather.js
+++ local-weather/javascripts/LocalWeather.js
@@ -1,6 +1,6 @@
 function LocalWeather(unitType) {
   var WEATHER_API_KEY = "87a3ac98e2e48918db144e9f69eeb057";
-  var localWeather = {
+  this.localWeather = {
         "city": "",
         "temp": 0,
         "humidity": 0,
@@ -12,11 +12,11 @@ function LocalWeather(unitType) {
       };
 
   this.getLocalWeather = function() {
-    return localWeather;
+    return this.localWeather;
   }
   this.getCity = function() {
-    console.log("get city: " + localWeather.city);
-    return localWeather.city;
+    console.log("get city: " + this.localWeather.city);
+    return this.localWeather.city;
   }
   this.setLocation = function() {
     navigator.geolocation.getCurrentPosition(success, error);
@@ -46,15 +46,28 @@ function LocalWeather(unitType) {
 
   function getWeatherFromApi(apiUrl) {
     //console.log(apiUrl);
-    $.getJSON(apiUrl, function(data){
-      console.log("in get weather");
-      // console.log(data.name);
-      localWeather.city = data.name;
-      // console.log(localWeather);
+    var result = $.getJSON(apiUrl, function(data){
+      // console.log(this);
+      // console.log("in get weather");
+      // // console.log(data.name);
+      // //this.localWeather = data.name;
+      // console.log(this.localWeather);
+      //console.log(data.responseText);
+      // console.log(data.responseText.coord);
+
+      // $.each(data, function(val){
+      //   console.log(val);
+      // });
+      return data;
     })
     .fail(function(jqxhr, status, error) {
       var err = status + ", " + error;
       alert("Sorry, the request failed: " + err);
     });
+    $.each(result, function(val){
+      console.log(val);
+    });
+    console.log(result);
+    // console.log(result.responseText);
   }
 } // end LocalWeather
