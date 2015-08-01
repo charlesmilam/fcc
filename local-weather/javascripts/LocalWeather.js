d1e8eca2b546f33755ae02109cfca02bc222256c
Charles Milam <charles.milam@gmail.com>
2015-08-01 05:44:37 -0500

    Modify LocalWeather, first working version.

--- local-weather/javascripts/LocalWeather.js
+++ local-weather/javascripts/LocalWeather.js
@@ -1,42 +1,49 @@
 var LocalWeather = function(unitType) {
   var WEATHER_API_KEY = "87a3ac98e2e48918db144e9f69eeb057";
-  var localWeather = {
-    "city": "",
-    "temp": 0,
-    "humidity": 0,
-    "sky": "",
-    "wind": {
-      "dir": 0,
-      "speed": 0
-    }
-  };
+  this.localWeather = {
+        "city": "",
+        "temp": 0,
+        "humidity": 0,
+        "sky": "",
+        "wind": {
+          "dir": 0,
+          "speed": 0
+        }
+      };
+  var apiUrl = "test";
 
-  var apiUrl = "";
+  this.setLocation = function() {
+    navigator.geolocation.getCurrentPosition(success, error);
 
-  this.getLocation = function() {
-    navigator.geolocation.getCurrentPosition(this.buildApiUrl);
+    function success(position) {
+      //console.log("in success");
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      console.log("before: " + apiUrl);
+      apiUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" +
+                lat +
+                "lon=" +
+                long +
+                "&units=" +
+                unitType +
+                "&APPID=" +
+                WEATHER_API_KEY;
+
+      console.log("after: " + apiUrl);
+      getWeather(apiUrl);
+    };
+
+    function error(error) {
+      alert(error);
+    };
   }
 
-  this.buildApiUrl = function(position) {
-    var lat = position.coords.latitude;
-    var long = position.coords.longitude;
-
-    apiUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" +
-              lat +
-              "lon=" +
-              long +
-              "&units=" +
-              unitType +
-              "&APPID=" +
-              WEATHER_API_KEY;
-
+  var getWeather = function(apiUrl) {
     console.log(apiUrl);
-  }
-
-  this.getWeather = function(apiUrl) {
     $.getJSON(apiUrl, function(data){
       console.log(data);
       this.localWeather.city = data.name;
+      console.log(data.name);
     })
     .fail(function(jqxhr, status, error) {
       var err = status + ", " + error;
