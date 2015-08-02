0bf7742d853e18df12cb334202f3cb12e3c0f3e0
Charles Milam <charles.milam@gmail.com>
2015-08-01 22:46:13 -0500

    Cleanup of comments and code.

--- local-weather/javascripts/getWeather.js
+++ local-weather/javascripts/getWeather.js
@@ -1,13 +1,5 @@
 var WEATHER_API_KEY = "87a3ac98e2e48918db144e9f69eeb057";
-var apiUrl = "";
-// var city = "";
-// var temp = 0;
-// var humidity = 0;
-// var sky = "";
-// var wind = {
-//   "dir": 0,
-//   "speed": 0
-// };
+// var apiUrl = "";
 
 function setLocation(unitType) {
   navigator.geolocation.getCurrentPosition(success, error);
@@ -17,14 +9,14 @@ function setLocation(unitType) {
     var long = position.coords.longitude;
     var latlng = new google.maps.LatLng(lat, long);
     var geocoder = new google.maps.Geocoder();
+    var apiUrl = "";
 
     geocoder.geocode({'latLng': latlng}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         if (results[1]) {
           for (var i = 0; i < results.length; i++) {
             if (results[i].types[0] === "locality") {
-              console.log(results[i].address_components[0].short_name);
-              // return results[i].address_components[0].short_name;
+              //console.log(results[i].address_components[0].short_name);
               apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" +
                         results[i].address_components[0].short_name +
                         "&units=" +
@@ -32,7 +24,7 @@ function setLocation(unitType) {
                         "&APPID=" +
                         WEATHER_API_KEY;
 
-              console.log("after: " + apiUrl);
+              // console.log("after: " + apiUrl);
               setWeatherFromApi(apiUrl);
             }
           }
@@ -41,16 +33,6 @@ function setLocation(unitType) {
       }
       else {console.log("Geocoder failed: " + status)}
     });
-    // console.log("city: ", city);
-    // apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" +
-    //           city +
-    //           "&units=" +
-    //           unitType +
-    //           "&APPID=" +
-    //           WEATHER_API_KEY;
-    //
-    // console.log("after: " + apiUrl);
-    // setWeatherFromApi(apiUrl);
   };
 
   function error(error) {
@@ -58,7 +40,7 @@ function setLocation(unitType) {
   };
 }
 
-function setWeatherFromApi() {
+function setWeatherFromApi(apiUrl) {
   $.getJSON(apiUrl, function(data){
     console.log(data);
     $(".city").text(data.name);
@@ -71,25 +53,5 @@ function setWeatherFromApi() {
   .fail(function(jqxhr, status, error) {
     var err = status + ", " + error;
     alert("Sorry, the request failed: " + err);
-  });
-}
-
-function getCity() {
-  latlng 	 = new google.maps.LatLng(lat, lng),
-  geocoder = new google.maps.Geocoder();
-  geocoder.geocode({'latLng': latlng}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      if (results[1]) {
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].types[0] === "locality") {
-            var city = results[i].address_components[0].short_name;
-            var state = results[i].address_components[2].short_name;
-            $("input[name='location']").val(city + ", " + state);
-          }
-        }
-      }
-      else {console.log("No reverse geocode results.")}
-    }
-    else {console.log("Geocoder failed: " + status)}
   });
 }
