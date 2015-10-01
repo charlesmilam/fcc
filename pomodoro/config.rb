145aedc35e09beaef998b8ba4dfb5c3e85f5b576
Charles Milam <charles.milam@gmail.com>
2015-10-01 00:12:20 -0500

    Restart project, using foundation cli to create project.

--- pomodoro/config.rb
+++ pomodoro/config.rb
@@ -1,12 +1,12 @@
-require 'compass/import-once/activate'
 # Require any additional compass plugins here.
+add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -16,7 +16,6 @@ javascripts_dir = "javascripts"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
