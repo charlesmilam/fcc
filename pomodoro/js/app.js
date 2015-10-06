dc61d4a186bbf690a958b795f178ba8bb74bcbcd
Charles Milam <charles.milam@gmail.com>
2015-10-05 22:13:58 -0500

    Add basic circular progress bar to index, making use of progressbar.js.

--- pomodoro/js/app.js
+++ pomodoro/js/app.js
@@ -1,3 +1,12 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+window.onload = function onLoad() {
+  var circle = new ProgressBar.Circle('#pomo-progress', {
+   color: '#FCB03C',
+   duration: 3000,
+   easing: 'easeInOut'
+  });
+
+  circle.animate(1);
+};
