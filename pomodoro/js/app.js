905b76d50c6a14f61c2cce6441bb14394e6d04a9
Charles Milam <charles.milam@gmail.com>
2015-10-08 21:23:50 -0500

    Add options for TimeCircles to app js

--- pomodoro/js/app.js
+++ pomodoro/js/app.js
@@ -1,3 +1,16 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// set TimeCircles options
+$("#pomo-progress").TimeCircles({
+  start: false,
+  animation: "smooth",
+  count_past_zero: false,
+  circle_bg_color: "#555555",
+  use_background: true,
+  fg_width: 0.05,
+  bg_width: 0.025,
+  total_duration: "Minutes",
+  direction: "Counter-clockwise"
+});
